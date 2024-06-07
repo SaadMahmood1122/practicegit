6fd45325db2fe25b216574b8bbcabdc7b5296eb5
SaadMahmood1122 <sk8066709@gmail.com>
2024-06-08 00:07:50 +0500

    removed unwanted data

--- demo.java
+++ demo.java
@@ -1,10 +1,6 @@
 public class Demo{
 public  void  m1(){
-<<<<<<< HEAD
-
-=======
     System.out.println("this is m1() method....");
->>>>>>> 62e5e0ae71ac1ac57f7f4cdd41804076dd81d43b
 }
 public static void main(String[] args) {
     System.out.println("Practicing the git");
