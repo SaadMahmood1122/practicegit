62e5e0ae71ac1ac57f7f4cdd41804076dd81d43b
SaadMahmood1122 <sk8066709@gmail.com>
2024-06-07 23:47:52 +0500

    changes made by saad

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class Demo{
 public  void  m1(){
-    
+    System.out.println("this is m1() method....");
 }
 public static void main(String[] args) {
     System.out.println("Practicing the git");
