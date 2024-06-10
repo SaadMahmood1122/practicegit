c241b120a5c8e58560a6c822920f6657c4d1ac2a
SaadMahmood1122 <sk8066709@gmail.com>
2024-06-10 17:45:50 +0500

    adding m2 method

--- demo.java
+++ demo.java
@@ -2,6 +2,9 @@ public class Demo{
 public  void  m1(){
     System.out.println("this is m1() method....");
 }
+public void m2(){
+    System.out.print("calling m2() method....")
+}
 public static void main(String[] args) {
     System.out.println("Practicing the git");
 }
